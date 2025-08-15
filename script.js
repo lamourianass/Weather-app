e3a92907cab25d61c5d2f9d9c67364034000a7ab
Anass Lamouri <anasslamouri@icloud.com>
2025-08-15 15:07:40 +0100

    Fix event in addEventListener in js file

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const weatherDataEl = document.getElementById('weather-data')
 const cityInputEl = document.getElementById('city-input')
 const formEl = document.querySelector('form')
 
-formEl.addEventListener('click', () => {})
+formEl.addEventListener('submit', () => {})
