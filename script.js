70c94a8cee4fa9975c8687985f1a01bcdb96fae1
Anass Lamouri <anasslamouri@icloud.com>
2025-08-15 15:06:18 +0100

    Add addEventListener in js file

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const apiKey = "fcc7518e67cb0ca5968f2ef774337981"
 const weatherDataEl = document.getElementById('weather-data')
 const cityInputEl = document.getElementById('city-input')
+const formEl = document.querySelector('form')
+
+formEl.addEventListener('click', () => {})
